60c9cdf3abe3e68988f000e596fd223c049865a8
742442849@qq.com <Lnk742442849>
2019-01-24 22:04:04 +0800

    完成Eureka集群配置，实现高可用

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 描述：完成消费者调用生产者（spring-cloud-study-consumer-dept-1 -> spring-cloud-study-provider-dept-1）
 #### 1.0.1.0123(20190123)
 描述：完成EurekaServer服务注册中心建立、完善主机映射名称修改、主机IP信息提示及info内容构建
+#### 1.0.3.0124(20190124)
+描述：完成Eureka集群配置，实现高可用
---
+++ spring-cloud-study-eureka3/src/main/java/ac/cn/saya/springcloud/study/eureka3/SpringCloudStudyEureka3Application.java
@@ -0,0 +1,35 @@
+package ac.cn.saya.springcloud.study.eureka3;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @描述
+ * @参数
+ * @返回值
+ * @创建人  saya.ac.cn-刘能凯
+ * @创建时间  2019/1/24
+ * @修改人和其它信息
+ * EurekaServer服务注册中心启动入口
+ */
+
+@SpringBootApplication
+@EnableEurekaServer // EurekaServer服务器端启动类,接受其它微服务注册进来
+public class SpringCloudStudyEureka3Application {
+
+	private static final Logger logger = Logger.getLogger(SpringCloudStudyEureka3Application.class);
+
+	public static void main(String[] args) {
+		/// SpringApplication.run(SpringCloudStudyEureka1Application.class, args);
+		SpringApplication springApplication = new SpringApplication(SpringCloudStudyEureka3Application.class);
+		// 禁止命令行设置参数
+		springApplication.setAddCommandLineProperties(false);
+		springApplication.run(args);
+		//项目启动完成打印项目名
+		logger.warn("服务注册中心Eureka3已经启动 ... ");
+	}
+
+}
+
